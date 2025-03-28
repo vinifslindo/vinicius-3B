09e78efee4165b7a5018501bce541e714cd64259
vinifslindo <134951528+vinifslindo@users.noreply.github.com>
2025-03-28 08:41:18 -0300

    28 03 2025

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+});
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    let tamanhoAtualFonte = 1;
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+
+    });
+
+});
+const botaoReduzirTamanho = document.getElementById("botaoReduzirTamanho");
+
+botaoReduzirTamanho.addEventListener("click", function () {
+  let letraTamanhoAtual = 1;
+  document.body.style.fontSize = `${letraTamanhoAtual}rem`;
+});
